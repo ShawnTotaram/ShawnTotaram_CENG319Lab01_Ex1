a09f39b50f8bea7b06b68dd9c3a94fd9370a0bae
ShawnTotaram <71612518+ShawnTotaram@users.noreply.github.com>
2020-09-24 23:32:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/shawntotaram_ceng319lab01_ex1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.shawntotaram_ceng319lab01_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+}
